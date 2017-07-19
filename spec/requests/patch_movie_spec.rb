a219348288c3fa8b3ce03a583a7f1e59545fcd96
Monique St. Laurent and Susha Dore <susha@>
2017-07-19 14:27:57 -0700

    pass patch route specs

---
+++ spec/requests/patch_movie_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "patch movies route", type: :request do
+  before do
+    movie = FactoryGirl.create(:movie)
+    put "/movies/#{movie.id}", params: {
+      :title => "new title",
+      :genre => "genre"
+    }
+  end
+
+  it 'should return status 200' do
+         expect(response.status).to eq 200
+     end
+
+
+  it do
+    expect(Movie.first.title).to eq "new title"
+  end
+
+end

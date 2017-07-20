93e3dde15a5d9060a1054a59c6f841d22b3e5b6b
Monique St. Laurent and Susha Dore <monique@>
2017-07-20 10:26:05 -0700

    pass spec for get individual quote

---
+++ spec/requests/get_quote_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "get quote path" do
+
+  before do
+    movie = Movie.create(title: 'test', genre: 'genre', id: 1)
+    quote = FactoryGirl.create(:quote)
+    get "/movies/#{movie.id}/quotes/#{quote.id}"
+  end
+
+  it 'returns a quote' do
+    expect(JSON.parse(response.body).size).to eq 6
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status :success
+  end
+
+end

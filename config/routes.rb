5bb2c4c990a3c7607d3f9393bc25d20167783da1
Monique St. Laurent and Susha Dore <susha@>
2017-07-20 09:24:01 -0700

    add pagination to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'movies#index'
-  resources :movies do
+  # root to: 'movies#index'
+
+  concern :paginatable do
+    get '(page/:page)', action: :index, on: :collection, as: ''
+  end
+
+  resources :movies, concerns: :paginatable do
     collection do
       get :most_quotes
     end
     resources :quotes
   end
+
 end
